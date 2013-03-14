a8ff2d0093ad14f646dbe8a52b96688950005ed7
Adam Griffiths <aogriffiths@gmail.com>
2013-03-14 06:55:11 +0000

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+note-wtr-dev-workflow-example-module
+====================================
+
+An example module for the note-wtr-dev-workflow-example
---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * @Author Adam Griffiths
+ * @description An example module for the note-wtr-dev-workflow-example.
+ */
+
+module.exports.sayhelloworld = function(){return 'Hello World!'};
